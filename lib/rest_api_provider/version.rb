d8684da07ae719ecfc5b7cfb043d8e392f6df007
Andrei Chernykh <andrei.chernykh@gmail.com>
2015-06-10 11:42:07 +0300

    initial commit

---
+++ lib/rest_api_provider/version.rb
@@ -0,0 +1,3 @@
+module RestApiProvider
+  VERSION = '0.1.0'
+end
---
+++ spec/rest_api_provider_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe RestApiProvider do
+  it 'has a version number' do
+    expect(RestApiProvider::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
